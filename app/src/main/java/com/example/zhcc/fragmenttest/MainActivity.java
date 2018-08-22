df65955315e259cb18c9b944e68d816114af69f9
ZHCC <zhuchuanchuan0904@gmail.com>
2018-08-22 15:13:09 +0800

    Fragment与Acitivity生命周期关系

--- app/src/main/java/com/example/zhcc/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/zhcc/fragmenttest/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.zhcc.fragmenttest;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+        repalceFragment(new RightFragment());
+    }
+
+    private void repalceFragment(Fragment fragment) {
+        FragmentManager fragmentManager=getFragmentManager();
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                repalceFragment(new AnotherRightFragment());
+                break;
+                default:
+                    break;
+        }
     }
 }
