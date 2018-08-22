822c8fc7afd29af93c15633a9bb72d07e97d16d4
ZHCC <zhuchuanchuan0904@gmail.com>
2018-08-22 10:38:14 +0800

    平板两栏扩展碎片布局

---
+++ app/src/main/java/com/example/zhcc/fragmenttest/LeftFragment.java
@@ -0,0 +1,21 @@
+package com.example.zhcc.fragmenttest;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Create by ZHCC on 2018/8/22
+ */
+public class LeftFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
+        return view;
+    }
+}
